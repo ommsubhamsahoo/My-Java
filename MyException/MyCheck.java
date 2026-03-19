63524eb958556e64a228be7518c3c14270a37ecf
ommsubham120-stack <ommsubham120@gmail.com>
2026-03-19 19:43:02 +0530

    initial commit

---
+++ MyException/MyCheck.java
@@ -0,0 +1,12 @@
+package MyException;
+
+import java.io.FileReader;
+
+public class MyCheck {
+public static void main(String[] args) {
+	/*FileReader fr=new FileReader("C:\\Users\\omm subham sahoo\\OneDrive\\Desktop\\simple");
+//	FileNotFoundException
+	fr.read();
+*/
+}
+}
